f53cdaf6c6434cedd7b02f6ad200b6f72a72c9a3
Hudson Cho <hudcho24@gmail.com>
2025-11-05 17:17:24 -0500

    Added database folder as well as schema.sql. This file can be used for deploying the database locally for testing or development purposes. Refer to README.md for more information

---
+++ Database/schema.sql
@@ -0,0 +1,37 @@
+create table users (
+	id serial primary key,
+	username TEXT unique not null,
+	password_hash TEXT not null 
+);
+
+create table friends (
+	user_id INT references users(id),
+	friend_id INT references users(id),
+	primary key (user_id, friend_id)
+);
+
+create table circles (
+	id serial primary key,
+	name TEXT unique not null,
+	created_by INT references users(id)
+);
+
+create table circle_members (
+	circle_id INT references circles(id),
+	user_id INT references users(id),
+	primary key (circle_id, user_id)
+);
+
+create table messages(
+	id serial primary key,
+	content TEXT not null,
+	sender_id INT references users(id),
+	recipient_id INT references users(id),
+	circle_id INT references circles(id),
+	sent_at TIMESTAMP default NOW(),
+	check ( 
+		(circle_id is not null and recipient_id is null) or
+		(circle_id is null and recipient_id is not null)
+	)
+);
+

35092e20d58fe947e024a1ec7239aee57c421e5c
Hudson Cho <hudcho24@gmail.com>
2025-11-16 18:23:03 -0500

    feat(circle_members): Add basic CRUD operations for circle_members
        - createCircleMember(user_id,circle_id): Add user to circle by
          linking their ids
        - getCircleMembers(circle_id):           Return members of a circle
        - deleteCircleMember(id)                 Remove user from
          circle_members table

---
+++ backend/db_connection/circle-members-db.js
@@ -0,0 +1,42 @@
+/*
+AUTHOR: Hudson Cho
+CREATED: 11.16.2025
+UPDATED: 11.16.2025
+DESCRIPTION:
+    The `circle-members-db.js` file is used to query the database about information regarding the member of a particular circle
+*/
+
+const pool = require('./pool.js');
+
+// Adds a member to a circle by linking the circle_id and user_id
+async function createCircleMember(circle_id, user_id) {
+    const result = await pool.query(
+        'INSERT INTO circle_members (circle_id, user_id) VALUES ($1, $2) RETURNING *',
+        [circle_id, user_id]
+    );
+    return result.rows[0];
+}
+
+// Returns the members of a specific circle
+async function getCircleMembers(circle_id) {
+    const result = await pool.query(
+        'SELECT * FROM circle_members WHERE circle_id=$1',
+        [circle_id]
+    );
+    return result.rows[0];
+}
+
+// Removes a member from a circle by removing the connection between circle_id and user_id from the database
+async function deleteCircleMember(id) {
+    const result = await pool.query(
+        'DELETE FROM circle_members WHERE id=$1 RETURNING *',
+        [id]
+    );
+    return result.rows[0]
+}
+
+module.exports={
+    createCircleMember,
+    getCircleMember,
+    deleteCircleMember
+}

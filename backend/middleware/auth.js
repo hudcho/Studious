fe7af247b0996b775c7ad5884df29f4f235a4bc3
Hudson Cho <hudcho24@gmail.com>
2025-11-20 18:02:02 -0500

    feat(auth): add auth.js to be middleware to authenticate a users JWT

---
+++ backend/middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require ('jsonwebtoken');
+const { JWT_SECRET } = require('../config/env');
+
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if(!token) {
+        return res.status(401).json({ error: 'No token provided'});
+    }
+
+    jwt.verify(token, JWT_SECRET, (err, user) => {
+        if (err) {
+            return res.status(403).json({ error: 'Token is invalid or expired'});
+        }
+        
+        req.user = user;
+        next();
+    });
+};
+
+module.exports = authenticateToken;

2ba50a9f3d8bee84b2b2c063103738ef2470927b
Hudson Cho <hudcho24@gmail.com>
2025-11-20 14:46:13 -0500

    feat(auth-route): add route for HTTP requests to authenticate

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const usersRoute = require('./routes/users-route');
+const authRoute = require('./routes/auth-route');
 
 const app = express();
 const PORT = 3000;
@@ -9,6 +10,8 @@ app.use(express.json());
 // Route HTTP requests to /users to users-route
 app.use('/users', usersRoute);
 
+// Route HTTP requests to /auth to auth-route
+app.use('/auth', authRoute)
 
 app.listen(PORT, () => {
     console.log('Server is running on http://localhost:${PORT}')

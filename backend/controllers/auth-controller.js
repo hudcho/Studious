c1b7c9739d01b477cda150dc29af74ab89cd03d9
Hudson Cho <hudcho24@gmail.com>
2025-11-20 14:33:57 -0500

    feature(auth-controller): create auth-controller and env.js
    create env.js to store JWT_SECRET
    
    add methods to auth-controller
    - login(): issue JWT if valid userame and password are entered

---
+++ backend/controllers/auth-controller.js
@@ -0,0 +1,39 @@
+/* 
+AUTHOR: Hudson Cho
+CREATED: 11.20.2025
+UPDATED: 11.20.2025
+DESCRIPTION:
+    The `auth-controller.js` file is used for authenticating a user using  
+*/
+
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const usersDb = require('../db_connectors/users-db');
+const { JWT_SECRET } = require('../config/env');
+
+async function login(req, res) {
+    const { username, password } = req.body;
+    const user = await usersDb.getPrivateUserByUsername(username);
+
+    // Checks user exists in database
+    if (!user){
+        return res.status(400).json({ error: 'Incorrect username or password' });
+    }
+
+    // Checks entered password against password in database
+    const correctPassword = await bcrypt.compare(password, user.password_hash);
+    if (!correctPassword) {
+        return res.status(400).json({ error: 'Incorrect username or password' });
+    }
+
+    // Create json web token
+    const token = jwt.sign(
+        { id:user.id, username:user.username },
+        JWT_SECRET,
+        { expiresIn: "1h" }
+    );
+
+    return res.status(200).json({ token });
+}
+
+module.exports = { login };

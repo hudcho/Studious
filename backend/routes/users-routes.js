fa83231220ca002a92ceebe8c227de621ab286ed
Hudson Cho <hudcho24@gmail.com>
2025-11-20 11:06:44 -0500

    feat(users-routes): add user-routes to route HTTP requests to users-controller

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -1,9 +1,26 @@
+/* 
+AUTHOR: Hudson Cho
+CREATED: 11.20.2025
+UPDATED: 11.20.2025
+DESCRIPTION:
+    The `users-routes.js` file is used for routing HTTP requests to the appropriate
+    handler located in users-controllers.js 
+*/
+
 const express = require('express');
 const router = express.Router();
-const usersController  = require('./controllers/users-controller.js');
+const usersController  = require('../controllers/users-controller');
 
-router.post('/', usersController.createUser);               // POST /users
+// POST /users
+router.post('/', usersController.createUser);               
 
-router.patch('/:id', usersController.updateUser)            // PATCH /users/:id
+// PATCH /users/:id
+router.patch('/:id', usersController.updateUser);            
+
+// GET /users/:id
+router.get('/:id', usersController.getUserById);
+
+// DELETE /users/:id
+router.delete('/:id', usersController.deleteUser);
 
 module.exports = router;

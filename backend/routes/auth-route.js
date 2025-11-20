44460a53b0072b8627fdb4124ccbee036ea9eabe
Hudson Cho <hudcho24@gmail.com>
2025-11-20 14:46:13 -0500

    feat(auth-route): add route for HTTP requests to authenticate

---
+++ backend/routes/auth-route.js
@@ -0,0 +1,15 @@
+/* 
+AUTHOR: Hudson Cho
+CREATED: 11.20.2025
+UPDATED: 11.20.2025
+DESCRIPTION:
+    The `users-routes.js` file is used for routing HTTP requests to the appropriate
+    handler located in users-controllers.js 
+*/
+
+const express = require('express');
+const router = express.Router();
+const authController  = require('../controllers/auth-controller');
+
+// Used for login 
+router.post('/login', authController.login);

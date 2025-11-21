bdb435ce6d874493a793165c1850ccfb31ba962b
Hudson Cho <hudcho24@gmail.com>
2025-11-20 20:53:34 -0500

    refactore(.gitignore): updating gitignore to hide secrets

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,3 +1,10 @@
+/* 
+AUTHOR: Hudson Cho
+CREATED: 11.20.2025
+UPDATED: 11.20.2025
+DESCRIPTION:
+    LoginPage page: allows for users to enter a username and password to login
+*/
 import React from "react";
 import LoginForm from "../components/LoginForm";
 

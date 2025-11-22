d189b765f849d1da81837c9f3c619b8cd9207669
Hudson Cho <hudcho24@gmail.com>
2025-11-22 00:10:35 -0500

    feat(server): add route files to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const usersRoute = require('./routes/users-route');
+const circlesRoute = require('./routes/circles-route');
+const messagesRoute = require('./routes/messages-route');
+const friendsRoute = require('./routes/friends-route');
+const membersRoute = require('./routes/members-route');
 const authRoute = require('./routes/auth-route');
 const cors = require('cors');
 
@@ -12,9 +16,24 @@ app.use(express.json());
 // Route HTTP requests to /users to users-route
 app.use('/users', usersRoute);
 
+// Route HTTP requests to /circles to circles-route
+app.use('/circles', circlesRoute)
+
+// Route HTTP requests to /messages to messages-route
+app.use('/messages', messagesRoute)
+
+// Route HTTP requests to /friends to friends-route
+app.use('/friends', friendsRoute)
+
+// Route HTTP requests to /members to members-route
+app.use('/members', membersRoute)
+
 // Route HTTP requests to /auth to auth-route
 app.use('/auth', authRoute)
 
+
+
+
 app.listen(PORT, () => {
-    console.log('Server is running on http://localhost:${PORT}')
+    console.log(`Server is running on http://localhost:${PORT}`)
 });

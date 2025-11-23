8338a062d86191a9a4c96091f0e2b7e33edaa9e3
Hudson Cho <hudcho24@gmail.com>
2025-11-22 21:23:37 -0500

    feature(socket-controller): add controller to facilitate real time socket.io communications

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
 const usersRoute = require('./routes/users-route');
 const circlesRoute = require('./routes/circles-route');
 const messagesRoute = require('./routes/messages-route');
@@ -7,11 +9,27 @@ const membersRoute = require('./routes/members-route');
 const authRoute = require('./routes/auth-route');
 const cors = require('cors');
 
+const { setupSocket } = require('./controllers/socket-controller');
+
 const app = express();
 const PORT = 3000;
 
+const server = http.createServer(app);
+
 app.use(cors());
 app.use(express.json());
+
+const io = new Server(server, {
+    cors: {
+        origin: 'http//localhost:3000',
+        methods: ['GET', 'POST' ]
+    }
+});
+
+setupSocket;
+
+
+
 
 // Route HTTP requests to /users to users-route
 app.use('/users', usersRoute);
@@ -30,6 +48,7 @@ app.use('/members', membersRoute)
 
 // Route HTTP requests to /auth to auth-route
 app.use('/auth', authRoute)
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
